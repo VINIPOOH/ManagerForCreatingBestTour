3cd3e40fde975a31a7fb28097f03f4fe2fed3442
Gtm417 <godik417@gmail.com>
2019-05-02 11:54:51 +0300

    sadsa
    выфвфы

---
+++ ManagerForCreatingBestTour/City.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagerForCreatingBestTour
+{
+    class City
+    {
+        public City(string name, int amountPeople, int amountPeopleYoungerTwenty)
+        {
+            Name = name;
+            AmountPeople = amountPeople;
+            AmountPeopleYoungerTwenty = amountPeopleYoungerTwenty;
+        }
+
+        public string Name { get; set; }
+        public int AmountPeople { get; set; }
+        public int AmountPeopleYoungerTwenty { get; set; }
+    }
+}
