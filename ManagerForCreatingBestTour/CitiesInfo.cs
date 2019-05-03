85d54c81d9cc58a8e68e9cdcd1914a4f82808c72
nikonov.sanynikonov@gmail.com <nikonov.sanynikonov@gmail.com>
2019-05-03 17:52:28 +0300

    Added class CitiesInfo, which contains information about several cities and distancies between them

---
+++ ManagerForCreatingBestTour/CitiesInfo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagerForCreatingBestTour
+{
+    /** 
+     * Stores information about cities. Includes:
+     * 1. Cities
+     * 2. Distances between cities
+     */
+    public static class CitiesInfo
+    {
+        public static City[] Cities()
+        {
+            City[] cities =
+            {
+                new City("Berlin", 3000000, 0),
+                new City("Kiev", 2000000, 0),
+                new City("Minsk", 500000, 0),
+                new City("Vien", 1000000, 0),
+                new City("Prague", 700000, 0),
+                new City("Warsaw", 1500000, 0),
+                new City("Budapesht", 600000, 0)
+            };
+
+            return cities;
+        }
+
+        public static int[,] Distances()
+        {
+            int inf = int.MaxValue;
+            int[,] distances = new int[7, 7]
+            {/*
+                            Berlin Kiev Minsk Vien Prague Warsaw Budapesht
+                Berlin
+                Kiev
+                Minsk
+                Vien
+                Prague
+                Warsaw
+                Budapesht
+
+                */
+                { 0, inf, inf, inf, 349, 575, inf},
+                { inf, 0, 569, inf, inf, 788, 1116},
+                { inf, 589, 0, inf, inf, 547, inf},
+                { inf, inf, inf, 0, 333, 683, 243},
+                { 349, inf, inf, 333, 0, 688, inf},
+                { 575, 788, 547, 683, 688, 0, 871},
+                { inf, 1116, inf, 243, inf, 871, 0}
+            };
+
+            return distances;
+        }
+
+    }
+}
---
+++ ManagerForCreatingBestTour/WayCreator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagerForCreatingBestTour
+{
+    class WayCreator
+    {
+        private int[,] distances;
+
+        public void Dijkstra(TwoWayLinkedList cities)
+        {
+            
+        }
+    }
+}
